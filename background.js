855c48182b984ede6136a83dd7eeb7ca16ea4847
jangkyungdo <rudeh0926@naver.com>
2020-06-19 15:27:42 +0900

    background update

--- background.js
+++ background.js
@@ -21,15 +21,14 @@
 
 // document.addEventListener("DOMContentLoaded", start);
 
-function randomBg() {
+document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body");
   const imgNumber = 5;
 
   const randomNumber = Math.floor(Math.random() * imgNumber);
   const image = new Image();
-  image.src = `/img/img${randomNumber + 1}.jpg`;
+  image.src = `img/img${randomNumber + 1}.jpg`;
   image.classList.add("background__image");
 
   body.appendChild(image);
-}
-document.addEventListener("DOMContentLoaded", randomBg);
+});
