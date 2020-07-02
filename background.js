857ee4bcb151908f07441bda09dba570db82bcf5
jangkyungdo <rudeh0926@naver.com>
2020-07-02 14:02:58 +0900

    file delete

--- background.js
+++ background.js
@@ -1,32 +1,10 @@
-// const body = document.querySelector("body");
-// const imgNumber = 5;
-
-// function createImage(randomNumber) {
-//   const image = new Image();
-//   image.src = `/img/img${randomNumber + 1}.jpg`;
-//   image.classList.add("background__image");
-//   body.appendChild(image);
-// }
-
-// function getRandom() {
-//   const random = Math.floor(Math.random() * imgNumber);
-
-//   return random;
-// }
-
-// function start() {
-//   const randomNumber = getRandom();
-//   createImage(randomNumber);
-// }
-
-// document.addEventListener("DOMContentLoaded", start);
-
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body");
   const imgNumber = 5;
 
   const randomNumber = Math.floor(Math.random() * imgNumber);
   const image = new Image();
+
   image.src = `img/img${randomNumber + 1}.jpg`;
   image.classList.add("background__image");
 
